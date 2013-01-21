3882a8d2ecf8fb9c53bc74cc5126922b22ff01b9
Marco Jahn <marco.jahn@gmail.com>
2013-01-21 21:04:55 +0100

    basisimplementierung für stores + modelle

---
+++ webapp/app/model/Project.js
@@ -0,0 +1,8 @@
+Ext.define('PHRS.model.Project', {
+    extend: 'PHRS.model.PhrsEntity',
+
+    fields: [
+        {name: 'erpId'},
+        {name: 'name'}
+    ]
+});
---
+++ webapp/app/store/Reservations.js
@@ -0,0 +1,5 @@
+Ext.define('PHRS.store.Reservations', {
+    extend: 'Ext.data.Store',
+
+    model: 'PHRS.model.Reservation'
+});
